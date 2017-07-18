7c276db533c4a368122dac050133937a47789b64
tomliuauto <tomliuauto@gmail.com>
2017-07-18 07:11:59 +0000

    revert to first commit

--- models/user.js
+++ models/user.js
@@ -2,14 +2,8 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: {type:String, unique:true,required:true},
+    username:String,
     password:String,
-    avatar:String,
-    firstName:String,
-    lastName: String,
-    email: {type:String, unique: true, required:true },
-    resetPasswordToken: String,
-    resetPasswordExpires:Date,
     isAdmin:{type:Boolean, default:false}
 });
 
