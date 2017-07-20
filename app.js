fe43d33d7138ef4e54c7f3ff78141c4887570ddc
tomliuauto <tomliuauto@gmail.com>
2017-07-20 17:57:28 +0000

    add mlab for online mongod database

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/texas_camp");
+//mongoose.connect("mongodb://localhost/texas_camp");
+mongoose.connect("mongodb://mike:888888@ds139781.mlab.com:39781/texascamp");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
